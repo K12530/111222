5382d5c0215e3816717b948270829cc959c76d26
yuankai <K1253042283@gmail.com>
2021-05-24 19:57:05 +0800

    提交1

---
+++ Project2_Git/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+int main()
+{
+	printf("%d\r\n",add(1, 2));
+	return 0;
+}
+
+int add(int a, int b)
+{
+	return a + b;
+}
---
+++ Project2_Git/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int add(int a, int b);
+
+#endif // !_MAIN_H_
